d64b5360a517cbe361d540e688323545ef961ebf
norcs <norbertcs@me.com>
2017-11-14 17:25:44 +0100

    fixed the board A-H 1-8

--- battleship.py
+++ battleship.py
@@ -21,31 +21,31 @@ def menu():
         exit()      
 
 def print_board(): #  prints the board
-    table1 = [" ", " ", " ", " ", " ", " ", " ", " "]
-    table2 = [" ", " ", " ", " ", " ", " ", " ", " "]
-    table3 = [" ", " ", " ", " ", " ", " ", " ", " "]
-    table4 = [" ", " ", " ", " ", " ", " ", " ", " "]
-    table5 = [" ", " ", " ", " ", " ", " ", " ", " "]
-    table6 = [" ", " ", " ", " ", " ", " ", " ", " "]
-    table7 = [" ", " ", " ", " ", " ", " ", " ", " "]
-    table8 = [" ", " ", " ", " ", " ", " ", " ", " "]
-    print("    A   B   C   D   E   F   G   H")
+    table = [[" ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " "]]
+    print("    1   2   3   4   5   6   7   8")
     print("  ╔═══╦═══╦═══╦═══╦═══╦═══╦═══╦═══╗")
-    print("1 ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║" % (table1[0], table1[1], table1[2],table1[3], table1[4], table1[5],table1[6], table1[7]))
+    print("A ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║" % (board[0][0], board[0][1], board[0][2],board[0][3], board[0][4], board[0][5],board[0][6], board[0][7]))
     print("  ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣")
-    print("2 ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║" % (table2[0], table2[1], table2[2],table2[3], table2[4], table2[5],table2[6], table2[7]))
+    print("B ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║" % (board[1][0], board[1][1], board[1][2],board[1][3], board[1][4], board[1][5],board[1][6], board[1][7]))
     print("  ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣")
-    print("3 ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║" % (table3[0], table3[1], table3[2],table3[3], table3[4], table3[5],table3[6], table3[7]))
+    print("C ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║" % (board[2][0], board[2][1], board[2][2],board[2][3], board[2][4], board[2][5],board[2][6], board[2][7]))
     print("  ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣")
-    print("4 ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║" % (table4[0], table4[1], table4[2],table4[3], table4[4], table4[5],table4[6], table4[7]))
+    print("D ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║" % (board[3][0], board[3][1], board[3][2],board[3][3], board[3][4], board[3][5],board[3][6], board[3][7]))
     print("  ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣")
-    print("5 ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║" % (table5[0], table5[1], table5[2],table5[3], table5[4], table5[5],table5[6], table5[7]))
+    print("E ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║" % (board[4][0], board[4][1], board[4][2],board[4][3], board[4][4], board[4][5],board[4][6], board[4][7]))
     print("  ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣")
-    print("6 ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║" % (table6[0], table6[1], table6[2],table6[3], table6[4], table6[5],table6[6], table6[7]))
+    print("F ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║" % (board[5][0], table6[1], table6[2],table6[3], table6[4], table6[5],table6[6], table6[7]))
     print("  ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣")
-    print("7 ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║" % (table7[0], table7[1], table7[2],table7[3], table7[4], table7[5],table7[6], table7[7]))
+    print("G ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║" % (board[6][0], board[6][1], board[6][2],board[6][3], board[6][4], board[6][5],board[6][6], board[6][7]))
     print("  ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣")
-    print("8 ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║" % (table8[0], table8[1], table8[2],table8[3], table8[4], table8[5],table8[6], table8[7]))
+    print("H ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║" % (board[7][0], board[7][1], board[7][2],board[7][3], board[7][4], board[7][5],board[7][6], board[7][7]))
     print("  ╚═══╩═══╩═══╩═══╩═══╩═══╩═══╩═══╝")
 
 
@@ -54,13 +54,12 @@ def placement(): #  placement of the ships
 
 
 def v_or_h(): #  vertical or horizontal placement
-    while(True):
-		user_input = input("vertical or horizontal (v,h) ? ")
-		if user_input == "v" or user_input == "h":
-			return user_input
+    while True:
+        user_input = input("vertical or horizontal (v,h) ? ")
+        if user_input == "v" or user_input == "h":
+            return user_input
 		else:
-			print "Invalid input. Please only enter v or h"
-
+            print("Invalid input. Please only enter v or h")
 
 
 def validate(): #  validate user ship placement( out of board, overplacement )
