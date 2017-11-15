fab2036eaf5f80993c6b9a32257dce323a31dca4
norcs <norbertcs@me.com>
2017-11-15 14:04:40 +0100

    deleted old code lines

--- battleship_old.py
+++ battleship_old.py
@@ -55,8 +55,7 @@ boardplayer_twoGuess= [
 ['0','0','0','0','0'],
 ['0','0','0','0','0']
 ]
-shipSize_3 = []
-shipSize_2 = []
+
 
 def v_or_h():
     
@@ -139,13 +138,12 @@ def placement_player_one(board, ships): # ship placement
             x,y = get_coor()
             orientation= v_or_h()
         board = place_ship(board, ships[ship], ori, x, y)
-
-    return board
+        return board
 
 def place_ship(board, ship, ori, x, y):
     
     for i in range(ship):
-        if orientation is 'v':
+        if ori is 'v':
             board[x+i][y] = '3'
         else:
             board[x][y+i] = '3' 
@@ -207,15 +205,6 @@ for i in range(1,2):
             print(player_1+' will start the game!')
 
 
-# MAIN
-turn = 0
-hitCount1=0
-hitCount2=0
-P1hitCount_Size2=0
-P1hitCount_Size3=0
-P2hitCount_Size2=0
-P2hitCount_Size3=0
-
 def main():
     
     ships = {"Battleship": 5, "Destroyer": 4, "Submarine": 3}
@@ -234,68 +223,7 @@ def main():
         print("It's your turn "+player_1)
         board = placement_player_one(board, ships)
         print_board(board)
-        print ('2/', P1hitCount_Size2)
-        print ('3/',  P1hitCount_Size3)
-        guess=input('Target coordinates: (x,y):')
-        parts=guess.split(',')
-        x = int(parts[0])
-        y = int(parts[1])
-        row = x - 1
-        coloumn = y - 1
-        P1ship2 = '2'
-        P1ship3 = '3'
-        if P1ship2 in boardplayer_two[row][coloumn]: # it searches for hits in the small ship first
-            print ('+++++++  HIT +++++++')
-            boardplacement_player_oneGuess[row][coloumn] = 'X' # it stores the 'Miss' value for visualizing later
-            P1hitCount_Size2 +=1
-        elif boardplayer_two[row][coloumn] == '0':
-            print ('------ MISS ------')
-            boardplacement_player_oneGuess[row][coloumn] = 'M'
-        elif P1ship3 in boardplayer_two[row][coloumn]: # it searches for hits in the big ship
-            print ('+++++++  HIT +++++++')
-            boardplacement_player_oneGuess[row][coloumn] = 'X'
-            P1hitCount_Size3 +=1
-        elif boardplayer_two[row][coloumn] == '0':
-            print ('------ MISS ------')
-            boardplacement_player_oneGuess[row][coloumn] = 'M'
-
-        if P1hitCount_Size2 + P1hitCount_Size3 == 5:
-            print ('\n+++++ ' + player_1 + ' WINS +++++')
-            break
-
-        
-        print("\nIt's your turn " +player_2)
-        for x in boardplayer_twoGuess:
-            print(*x)
-        print ('2/', P2hitCount_Size2)
-        print ('3/', P2hitCount_Size3)
-        guess2=input('Target coordinates: (x,y):')
-        parts2=guess2.split(',')
-        x2 = int(parts2[0])
-        y2 = int(parts2[1])
-        row2 = x2 - 1
-        coloumn2 = y2 - 1
-        P2ship2 = '2'
-        P2ship3 = '3'
-        if P2ship2 in boardplacement_player_one[row2][coloumn2]:
-            print ('+++++++  HIT +++++++')
-            boardplayer_twoGuess[row2][coloumn2] = 'X'
-            P2hitCount_Size2 +=1
-        elif boardplacement_player_one[row2][coloumn2] == '0':
-            print ('------ MISS ------')
-            boardplayer_twoGuess[row2][coloumn2] = 'M'
-        elif P2ship3 in boardplacement_player_one[row2][coloumn2]:
-            print ('+++++++  HIT +++++++')
-            boardplayer_twoGuess[row2][coloumn2] = 'X'
-            P2hitCount_Size3 +=1
-        elif boardplacement_player_one[row2][coloumn2] == '0':
-            print ('------ MISS ------')
-            boardplayer_twoGuess[row2][coloumn2] = 'M'
-        
-        if P2hitCount_Size2 + P2hitCount_Size3 == 5:
-            print ('\n+++++++ ' + player_2 + ' WINS +++++++')
-            
-            break
+        break
 
 if __name__=="__main__":
     	main()
