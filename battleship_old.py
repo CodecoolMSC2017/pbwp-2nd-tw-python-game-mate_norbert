5df1b4a460c7e80d405051664dbee3dfcb8aa76d
norcs <norbertcs@me.com>
2017-11-15 19:02:26 +0100

    UI fixes

--- battleship_old.py
+++ battleship_old.py
@@ -35,30 +35,38 @@ def menu(players):
 
 def print_board(table):
     board = table
-    print("   ╔════════════════════════════╗ ")
-    print("   ║ Battleship has a size of 5 ║ ")
-    print("   ║ Destroyer has a size of 4  ║ ")
-    print("   ║ Submarine has a size of 3  ║ ")
-    print("   ╚════════════════════════════╝ ")
+    print("         ╔════════════════════════════╗ ")
+    print("         ║ Battleship has a size of 5 ║ ")
+    print("         ║ Destroyer has a size of 4  ║ ")
+    print("         ║ Submarine has a size of 3  ║ ")
+    print("         ╚════════════════════════════╝ ")
 
-    print("    1   2   3   4   5   6   7   8")
-    print("   ╔═══╦═══╦═══╦═══╦═══╦═══╦═══╦═══╗")
-    print(" A ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║" % (board[0][0], board[0][1], board[0][2], board[0][3], board[0][4], board[0][5], board[0][6], board[0][7]))
-    print("   ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣")
-    print(" B ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║" % (board[1][0], board[1][1], board[1][2], board[1][3], board[1][4], board[1][5], board[1][6], board[1][7]))
-    print("   ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣")
-    print(" C ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║" % (board[2][0], board[2][1], board[2][2], board[2][3], board[2][4], board[2][5], board[2][6], board[2][7]))
-    print("   ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣")
-    print(" D ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║" % (board[3][0], board[3][1], board[3][2], board[3][3], board[3][4], board[3][5], board[3][6], board[3][7]))
-    print("   ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣")
-    print(" E ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║" % (board[4][0], board[4][1], board[4][2], board[4][3], board[4][4], board[4][5], board[4][6], board[4][7]))
-    print("   ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣")
-    print(" F ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║" % (board[5][0], board[5][1], board[5][2], board[5][3], board[5][4], board[5][5], board[5][6], board[5][7]))
-    print("   ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣")
-    print(" G ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║" % (board[6][0], board[6][1], board[6][2], board[6][3], board[6][4], board[6][5], board[6][6], board[6][7]))
-    print("   ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣")
-    print(" H ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║" % (board[7][0], board[7][1], board[7][2], board[7][3], board[7][4], board[7][5], board[7][6], board[7][7]))
-    print("   ╚═══╩═══╩═══╩═══╩═══╩═══╩═══╩═══╝")
+    print("          1   2   3   4   5   6   7   8")
+    print("        ╔═══╦═══╦═══╦═══╦═══╦═══╦═══╦═══╗")
+    print("      A ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║" % (board[0][0], board[0][1], 
+    board[0][2], board[0][3], board[0][4], board[0][5], board[0][6], board[0][7]))
+    print("        ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣")
+    print("      B ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║" % (board[1][0], board[1][1], 
+    board[1][2], board[1][3], board[1][4], board[1][5], board[1][6], board[1][7]))
+    print("        ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣")
+    print("      C ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║" % (board[2][0], board[2][1],
+     board[2][2], board[2][3], board[2][4], board[2][5], board[2][6], board[2][7]))
+    print("        ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣")
+    print("      D ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║" % (board[3][0], board[3][1],
+     board[3][2], board[3][3], board[3][4], board[3][5], board[3][6], board[3][7]))
+    print("        ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣")
+    print("      E ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║" % (board[4][0], board[4][1],
+     board[4][2], board[4][3], board[4][4], board[4][5], board[4][6], board[4][7]))
+    print("        ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣")
+    print("      F ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║" % (board[5][0], board[5][1],
+     board[5][2], board[5][3], board[5][4], board[5][5], board[5][6], board[5][7]))
+    print("        ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣")
+    print("      G ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║" % (board[6][0], board[6][1],
+     board[6][2], board[6][3], board[6][4], board[6][5], board[6][6], board[6][7]))
+    print("        ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣")
+    print("      H ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║ %s ║" % (board[7][0], board[7][1],
+     board[7][2], board[7][3], board[7][4], board[7][5], board[7][6], board[7][7]))
+    print("        ╚═══╩═══╩═══╩═══╩═══╩═══╩═══╩═══╝")
 
 
 def v_or_h():
@@ -139,7 +147,7 @@ def validate(board, ships, ship, ori, x, y):
     return True
 
 
-def placement(board, ships): # ship placement
+def placement(board, ships, players): # ship placement
     cls()
     for ship in ships.keys():
         print_board(board)
